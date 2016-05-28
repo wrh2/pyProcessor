578c742d8bf72e86ac9d04555a4cb59ae51f5302
wrh2 <wrh2@pdx.edu>
2016-05-28 12:55:37 -0700

    exapnded RAM to have three ports for output, only the first port can be written to, modified testbench accordingly and generated new example output

--- code/Memory/RAM/RAM.py
+++ code/Memory/RAM/RAM.py
@@ -7,17 +7,19 @@
 from myhdl import *
 from random import randrange
 
-def RAM(dout, din, we, addr, clk):
+def RAM(dout0, dout1, dout2, din, we, addr0, addr1, addr2, clk):
 
-	mem = [Signal(modbv(0)[16:]) for x in range(16)]
+	mem = [Signal(modbv(0)[16:]) for x in range(2**8)]
 
 	@always(clk.posedge)
 	def write():
 		if we:
-			mem[int(addr)].next = din
+			mem[int(addr0)].next = din
 
 	@always_comb
 	def read():
-		dout.next = mem[int(addr)]
+		dout0.next = mem[int(addr0)]
+		dout1.next = mem[int(addr1)]
+		dout2.next = mem[int(addr2)]
 
 	return write, read
--- code/Memory/RAM/testbench.py
+++ code/Memory/RAM/testbench.py
@@ -3,13 +3,15 @@ from myhdl import *
 from RAM import RAM
 
 def RAM_testbench():
-	din, dout, addr = [Signal(modbv(0)[16:]) for i in range(3)]
-	we,clk = [Signal(bool(0)) for i in range(2)]
+        dout0, dout1, dout2 = [Signal(modbv(0)[16:]) for i in range(3)]
+	addr0, addr1, addr2 = [Signal(modbv(0)[8:]) for i in range(3)]
+	din = Signal(modbv(0)[16:])
+	we, clk = [Signal(bool(0)) for i in range(2)]
 
 	count = Signal(modbv(0)[4:])
 	cycle = Signal(modbv(0)[4:])
 
-	ram_inst = RAM(dout, din, we, addr, clk)
+	ram_inst = RAM(dout0, dout1, dout2, din, we, addr0, addr1, addr2, clk)
 
 	@always(delay(10))
 	def clkgen():
@@ -23,7 +25,9 @@ def RAM_testbench():
 
 	@always(clk.posedge)
 	def stimulus():
-		addr.next = modbv(count.val)[16:]
+		addr0.next = modbv(count.val)[16:]
+		addr1.next = modbv(count.val)[16:]
+		addr2.next = modbv(count.val)[16:]
 		din.next = modbv(count.val)[16:]
 
 		if cycle < 1:
