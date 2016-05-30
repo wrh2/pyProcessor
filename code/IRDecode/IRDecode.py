a0161c72b1ec64fdc4dd25c3d05998863e04f177
wrh2 <wrh2@pdx.edu>
2016-05-29 20:11:10 -0700

    got rid of internal signals on IR that were causing an extra cycle delay

--- code/IRDecode/IRDecode.py
+++ code/IRDecode/IRDecode.py
@@ -10,65 +10,52 @@ def IRDecode(Ins, J, BEQ, BZ, BP, BN, notHalt, NOP, op, Addr0, Addr1, Addr2, clk
 	"""
 	Module for Instruction Register/Decoder
 	"""
-	internal_op = Signal(modbv(0)[8:])
-	internal_addr0 = Signal(modbv(0)[8:])
-	internal_addr1 = Signal(modbv(0)[8:])
-	internal_addr2 = Signal(modbv(0)[8:])
-
 	@always(clk.posedge)
 	def latch_instruction():
-		internal_op.next = Ins[32:24]
-		internal_addr0.next = Ins[24:16]
-		internal_addr1.next = Ins[16:8]
-		internal_addr2.next = Ins[8:0]
-
-	@always(clk.posedge)
-	def pass_on():
-
 		# outputs
-		op.next = internal_op
-		Addr0.next = internal_addr0
-		Addr1.next = internal_addr1
-		Addr2.next = internal_addr2
+		op.next = Ins[32:24]
+		Addr0.next = Ins[24:16]
+		Addr1.next = Ins[16:8]
+		Addr2.next = Ins[8:0]
 
 		# branch if zero
-		if internal_op == 0x8:
+		if op == 0x8:
 			BZ.next = bool(1)
 		else:
 			BZ.next = bool(0)
 
 		# branch if equal
-		if internal_op == 0x9:
+		if op == 0x9:
 			BEQ.next = bool(1)
                 else:
 			BEQ.next = bool(0)
 
 		# branch is positive
-		if internal_op == 0xA:
+		if op == 0xA:
 			BP.next = bool(1)
                 else:
 			BP.next = bool(0)
 
 		# branch if negative
-		if internal_op == 0xB:
+		if op == 0xB:
 			BN.next = bool(1)
                 else:
 			BN.next = bool(0)
 
 		# jump
-		if internal_op == 0xC:
+		if op == 0xC:
 			J.next = bool(1)
                 else:
 			J.next = bool(0)
 
 		# halt
-		if internal_op == 0xD:
+		if op == 0xD:
 			notHalt.next = bool(0)
                 else:
 			notHalt.next = bool(1)
 
 		# no op
-		if internal_op == 0xE:
+		if op == 0xE:
 			NOP.next = bool(1)
                 else:
 			NOP.next = bool(0)
